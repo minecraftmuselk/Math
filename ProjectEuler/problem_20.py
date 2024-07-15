be4a365ac68df925a12f5867dc75ffe118a119dc
Finan Illikkal <finani007@gmail.com>
2024-07-15 17:49:39 -0400

    Problem 20

---
+++ ProjectEuler/problem_20.py
@@ -0,0 +1,16 @@
+import math
+
+facts = []
+for i in range(1, 101):
+    facts.append(i)
+
+factorial = math.prod(facts)
+factorial = str(factorial)
+add = 0
+
+for i in factorial:
+    i = int(i)
+    add += i
+
+print(factorial)
+print(add)
