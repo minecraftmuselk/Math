33acae9787f4c715159532209b0e2ecd82f1d4d4
Finan Illikkal <finani007@gmail.com>
2024-07-15 17:50:52 -0400

    Reformatted Project

--- ProjectEuler/problem_1.py
+++
@@ -1,8 +0,0 @@
-"""Find the sum of all the multiples of 3 or 5 below 1000."""
-
-total_sum = 0
-for i in range(1000):
-    if i % 3 == 0 or i % 5 == 0:
-        total_sum = total_sum+i
-
-print(total_sum)
