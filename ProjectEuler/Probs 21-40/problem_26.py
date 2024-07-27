63f699ab1426b83b25be6338e0c8aeb54d81d997
Finan Illikkal <55005671+minecraftmuselk@users.noreply.github.com>
2024-07-27 15:26:41 -0400

    problem_26

---
+++ ProjectEuler/Probs 21-40/problem_26.py
@@ -0,0 +1,26 @@
+def recurring_cycle_length(d):
+    remainders = {}
+    value = 1
+    position = 0
+
+    while value != 0 and value not in remainders:
+        remainders[value] = position
+        value *= 10
+        value %= d
+        position += 1
+
+    if value != 0:
+        return position - remainders[value]
+    else:
+        return 0
+
+max_length = 0
+number = 0
+
+for d in range(1, 1000):
+    cycle_length = recurring_cycle_length(d)
+    if cycle_length > max_length:
+        max_length = cycle_length
+        number = d
+
+print(f"The number with the longest recurring cycle is {number} with a cycle length of {max_length}.")
