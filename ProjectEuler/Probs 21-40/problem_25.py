14382a66eeab69116c0b146341dfcc8f24428c57
Finan Illikkal <55005671+minecraftmuselk@users.noreply.github.com>
2024-07-27 14:30:24 -0400

    problem_25

--- ProjectEuler/Probs 21-40/problem_25.py
+++ ProjectEuler/Probs 21-40/problem_25.py
@@ -1,4 +1,15 @@
-list_numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-element = 3
-my = list_numbers.index(element)
-print(my)
+a = 1
+b = 1
+
+fib_seq = [a, b]
+for i in range(1, 100_000):
+    next_number = fib_seq[-1] + fib_seq[-2]
+    fib_seq.append(next_number)
+
+for i in fib_seq:
+    if len(str(i)) == 999:
+        my_num = fib_seq.index(i)
+        print(my_num)
+        break
+
+print(fib_seq)
