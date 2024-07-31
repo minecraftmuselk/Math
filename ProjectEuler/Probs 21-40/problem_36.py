069f8be7073dde7bba24fa6f5f5847890015b8f2
Finan Illikkal <55005671+minecraftmuselk@users.noreply.github.com>
2024-07-31 11:53:34 -0400

    problem_34

---
+++ ProjectEuler/Probs 21-40/problem_36.py
@@ -0,0 +1,25 @@
+def convert_to_binary(n):
+    return bin(n)[2:]
+
+
+def is_palindrome(n):
+    n = str(n)
+    reverse = n[::-1]
+    if reverse == n:
+        return True
+    else:
+        return False
+
+
+def main():
+    palindromic = []
+    for i in range(1, 1_000_001):
+        b = convert_to_binary(i)
+        if is_palindrome(i) and is_palindrome(b):
+            palindromic.append(i)
+        else:
+            pass
+    return sum(palindromic)
+
+
+print(main())
